a4c800eaf43e8bf8f111068715a91b2987e3c642
Ilya Skryabin <808lorem@gmail.com>
2017-05-27 23:28:54 +0300

    Добавил вверхний слайдер

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,5 +1,17 @@
 ;
 // Начинать писать отсюда!!!!
 $(document).ready(function(){
+	$('.header__nav a').click(function() {
+		$('.active').removeClass('active');
+		$(this).addClass('active');
+	});
 	
+	
+	$('.js-slick__slider').slick({
+		dots: true,
+		infinite: false,
+		speed: 300,
+		nextArrow: '.slider__arrow_next',
+		prevArrow: '.slider__arrow_prev',
+	});
 });
