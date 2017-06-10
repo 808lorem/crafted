a74d6ab83986a7d1e040681a39330967e54f05ba
Ilya Skryabin <808lorem@gmail.com>
2017-06-10 17:42:36 +0300

    Добавил сайдбар

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,5 +1,29 @@
 ;
 // Начинать писать отсюда!!!!
+
+var htmlName = window.location.pathname;
+
+	$(".header__nav a").each(function(){
+		var hash = $(this).attr("href");
+		if('/' + hash == htmlName) {
+			$(".header__nav a.active").removeClass("active");
+			$(this).addClass("active");
+		}
+		
+		if(htmlName == '/index.html' || htmlName == '/') {
+			$(".sidebar").css('display', 'none');
+			$(".slider.mini").removeClass("mini");
+			$(".slider-bt__box.mini").removeClass("mini");
+			$(".js-slick__slider-bt_mini").attr('class', 'js-slick__slider-bt');
+		} else {
+			$(".sidebar").css('display', 'block');
+			$(".slider").addClass("mini");
+			$(".slider-bt__box").addClass("mini");
+			$(".js-slick__slider-bt").attr('class', 'js-slick__slider-bt_mini');
+		}
+	});
+
+
 $(document).ready(function(){
 	$('.header__nav a').click(function() {
 		$('.active').removeClass('active');
@@ -19,6 +43,15 @@ $(document).ready(function(){
 		speed: 300,
 		slidesToShow: 6,
 		slidesToScroll: 6,
+		nextArrow: '.slider-bt__arrow_next',
+		prevArrow: '.slider-bt__arrow_prev',
+	});
+	
+	$('.js-slick__slider-bt_mini').slick({
+		dots: false,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 4,
 		nextArrow: '.slider-bt__arrow_next',
 		prevArrow: '.slider-bt__arrow_prev',
 	});
